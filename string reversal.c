733066d747424e31eddbcf76dbece6da69b4b882
Eagerwolverine07 <124252528+Eagerwolverine07@users.noreply.github.com>
2023-04-25 14:34:37 +0530

    Add files via upload

---
+++ string reversal.c
@@ -0,0 +1,17 @@
+// to reverse a string given by user.
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int am;int i;
+    char str[20];
+    printf("Enter the string to be reversed: \n");
+    scanf("%s",&str);
+    am=strlen(str); //Calculating the length of string entered
+    
+    printf("Reversed string is: \n");
+    for(i=am-1;i>=0;i--) // Backward loop applied to print the array in reverse.
+    {
+        printf("%c",str[i]);
+    }
+}
